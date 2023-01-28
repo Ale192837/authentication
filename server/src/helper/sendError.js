934535e386a7cd1a2ff0ef94789a3d79aac0684f
Ale192837 <alexandre.2018@alunos.utfpr.edu.br>
2023-01-28 04:34:03 -0300

    add logger

--- server/src/helper/sendError.js
+++ server/src/helper/sendError.js
@@ -1,6 +1,12 @@
-module.exports = (req, res, next) => {
-    res.sendError = function(err){
+const logger = require('@service/logger');
 
+module.exports = (req, res, next) => {
+
+    res.sendError = function(err){
+        
+        logger.error({
+            err
+        })
         return this.err;
     
     }
